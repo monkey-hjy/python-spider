edf93019ff8c67844d6f50031abf72e84352dc93
monkey-hjy <50664775+monkey-hjy@users.noreply.github.com>
2021-04-23 17:02:59 +0800

    获取粉丝信息

--- weibo/get_fans_info.py
+++ weibo/get_fans_info.py
@@ -3,11 +3,13 @@
 # @Date:   2021-04-22 11:32:21
 # @Last Modified by:   monkey-hjy
 # @Last Modified time: 2021-04-22 16:57:22
+from gevent import monkey; monkey.patch_all()
+import gevent.pool
 import json
-
 import requests
 import random
 import re
+import time
 
 
 class GetFansInfo(object):
@@ -22,28 +24,55 @@ class GetFansInfo(object):
         self._headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
         }
+        self.get_cookie()
 
     def get_response(self, url):
         """解析到对应URL的response"""
-        response = requests.get(url, headers=self._headers)
-        if response.status_code == 200:
-            response.encoding = response.apparent_encoding
-            return response
-        else:
-            return None
+        err_count = 0
+        while err_count < 3:
+            try:
+                response = requests.get(url, headers=self._headers)
+                if response.status_code == 200:
+                    response.encoding = 'utf8'
+                    if '<title>Sina Visitor System</title>' in response.text:
+                        raise Exception
+                    return response
+                else:
+                    raise Exception
+            except:
+                err_count += 1
+                self.get_cookie()
+        return None
 
-    def get_fans_info(self, user_id):
+    def get_fans_info(self, user_info):
         """获取粉丝的信息"""
-        self.get_cookie()
-        response = self.get_response(url=self.get_info_url.format(user_id))
-        city = re.findall(r'所在地：.*?pt_detail\\">(.*?)<', response.text)[0]
-        gender = re.findall(r'性别：.*?pt_detail\\">(.*?)<', response.text)[0]
-        reg_date = re.findall(r'注册时间：.*?pt_detail\\">(.*?)<',
-                              response.text)[0].replace('\\n', '').replace('\\r', '').strip()
-        return city, gender, reg_date
+        user_info = user_info['user']
+        response = self.get_response(url=self.get_info_url.format(user_info['id']))
+        if response is None:
+            print('出错 === {}'.format(user_info))
+            return
+        city = re.findall(r'所在地：.*?pt_detail\\">(.*?)<', response.text)
+        city = city[0] if city else '其他'
+        gender = re.findall(r'性别：.*?pt_detail\\">(.*?)<', response.text)
+        gender = gender[0] if gender else '未知'
+        reg_date = re.findall(r'注册时间：.*?pt_detail\\">(.*?)<', response.text)
+        reg_date = reg_date[0].replace('\\n', '').replace('\\r', '').strip() if reg_date else '未知'
+        item = {
+            "the_fans_id": user_info['id'],
+            "screen_name": user_info['screen_name'],
+            "followers_count": user_info['followers_count'],
+            "follow_count": user_info['follow_count'],
+            "gender": gender,
+            "city": city,
+            "reg_date": reg_date
+        }
+        # print(item)
+        with open('weibo.json', encoding='utf8', mode='a') as f:
+            f.write(json.dumps(item, ensure_ascii=False))
 
     def get_fans_id(self, user_id, since_id=0):
         """获取到某个用户的粉丝"""
+        print(user_id, since_id)
         response = self.get_response(url=self.get_fans_url.format(user_id, since_id))
         if response is None:
             print('哥们。这个用户解析好像有点问题....')
@@ -52,18 +81,11 @@ class GetFansInfo(object):
             print('哥们。这个用户解析好像有点问题....')
             return
         fans_info_list = response.json()['data']['cards'][-1]['card_group']
-        for info in fans_info_list:
-            city, gender, reg_date = self.get_fans_info(user_id=info['user']['id'])
-            item = {
-                "the_fans_id": info['user']['id'],
-                "screen_name": info['user']['screen_name'],
-                "followers_count": info['user']['followers_count'],
-                "follow_count": info['user']['follow_count'],
-                "gender": gender,
-                "city": city,
-                "reg_date": reg_date
-            }
-            print(item)
+        pool = gevent.pool.Pool()
+        pool.map(self.get_fans_info, fans_info_list)
+        next_since_id = response.json()['data']['cardlistInfo']['since_id']
+        if next_since_id:
+            self.get_fans_id(user_id=user_id, since_id=next_since_id)
 
     @staticmethod
     def get_tid():
