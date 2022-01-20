ce6ddae5dced50374b857a54199d8c64bd27e8eb
monkey <847703187@qq.com>
2022-01-20 09:40:14 +0800

    tweet更新token获取方式

--- tweet/GetToken.py
+++ tweet/GetToken.py
@@ -4,29 +4,29 @@
 # @Last Modified by:   monkey-hjy
 # @Last Modified time: 2021-02-24 17:20:32
 import requests
-import re
 
 
 class GetToken(object):
     """获取到游客token"""
     def __init__(self):
-        self.get_token_url = 'http://mobile.twitter.com/'
+        self.get_token_url = 'https://api.twitter.com/1.1/guest/activate.json'
         self.get_token_headers = {
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-            'Upgrade-Insecure-Requests': '1',
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+            'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
         }
 
     def get_token(self, proxies_ip):
         proxies = {
-            'http': f'http://{proxies_ip}',
-            'https': f'http://{proxies_ip}',
+            'http': 'http://{}'.format(proxies_ip),
+            'https': 'http://{}'.format(proxies_ip),
         }
         err_count = 0
         while err_count < 5:
             try:
-                response = requests.get(url=self.get_token_url, headers=self.get_token_headers, timeout=10,
-                                        proxies=proxies)
-                result = re.findall(r'document.cookie.*gt=(.*?); Max-Age', response.text)
-                return result[0]
-            except:
+                response = requests.request(url=self.get_token_url, method="POST", headers=self.get_token_headers,
+                                            timeout=15)
+                response.close()
+                return response.json().get('guest_token')
+            except Exception as e:
+                print(e)
                 err_count += 1
